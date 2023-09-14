bb1c81a1f621fbf8d78c0093d20a5df0d372e73a
manishapillai <mvpilla@wisc.edu>
2023-09-14 15:12:08 -0500

    add imgview

--- app/src/main/java/com/cs407/helloworld/MainActivity.java
+++ app/src/main/java/com/cs407/helloworld/MainActivity.java
@@ -3,9 +3,10 @@ package com.cs407.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
